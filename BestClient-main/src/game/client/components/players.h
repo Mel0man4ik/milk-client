d9192e9a7b1e2c0de6a1397839dbb28f3f219170
Mel0man4ik <mishanyide32@gmail.com>
2026-08-31 05:40:27 +1000

    Добавил исходный код BestClient

---
+++ BestClient-main/src/game/client/components/players.h
@@ -0,0 +1,102 @@
+/* (c) Magnus Auvinen. See licence.txt in the root of the distribution for more information. */
+/* If you are missing that file, acquire a complete release at teeworlds.com.                */
+
+#ifndef GAME_CLIENT_COMPONENTS_PLAYERS_H
+#define GAME_CLIENT_COMPONENTS_PLAYERS_H
+
+#include <generated/protocol.h>
+
+#include <game/client/component.h>
+#include <game/client/render.h>
+
+class CPlayers : public CComponent
+{
+    friend class CGhost;
+
+    void RenderHand6(const CTeeRenderInfo *pInfo, vec2 HandPos, float HandAngle, float Alpha);
+    void RenderHand7(const CTeeRenderInfo *pInfo, vec2 HandPos, float HandAngle, float Alpha);
+
+    void RenderHand(
+        const CTeeRenderInfo *pInfo,
+        vec2 CenterPos,
+        vec2 Dir,
+        float AngleOffset,
+        vec2 PostRotOffset,
+        float Alpha);
+
+    void RenderPlayer(
+        const CNetObj_Character *pPrevChar,
+        const CNetObj_Character *pPlayerChar,
+        const CTeeRenderInfo *pRenderInfo,
+        int ClientId,
+        float Intra = 0.f);
+
+    void RenderPlayerGhost(
+        const CNetObj_Character *pPrevChar,
+        const CNetObj_Character *pPlayerChar,
+        const CTeeRenderInfo *pRenderInfo,
+        int ClientId,
+        float Intra = 0.f);
+
+    void RenderHook(
+        const CNetObj_Character *pPrevChar,
+        const CNetObj_Character *pPlayerChar,
+        const CTeeRenderInfo *pRenderInfo,
+        int ClientId,
+        float Intra = 0.f);
+
+    void RenderHookCollLine(
+        const CNetObj_Character *pPrevChar,
+        const CNetObj_Character *pPlayerChar,
+        int ClientId,
+        const vec2 *pOverridePosition = nullptr,
+        const vec2 *pOverrideDirection = nullptr,
+        const float *pOverrideLength = nullptr);
+
+    bool IsPlayerInfoAvailable(int ClientId) const;
+
+    int m_WeaponEmoteQuadContainerIndex;
+    int m_aWeaponSpriteMuzzleQuadContainerIndex[NUM_WEAPONS];
+
+    void CreateNinjaTeeRenderInfo();
+    void CreateSpectatorTeeRenderInfo();
+
+    std::shared_ptr<CManagedTeeRenderInfo> m_pNinjaTeeRenderInfo;
+    std::shared_ptr<CManagedTeeRenderInfo> m_pSpectatorTeeRenderInfo;
+
+    void RenderTeeLines();
+
+    vec2 m_aLastTeePos[MAX_CLIENTS] = {};
+    bool m_aLastTeePosValid[MAX_CLIENTS] = {};
+    bool m_aDeadTee[MAX_CLIENTS] = {};
+    bool m_aTeeWasInactive[MAX_CLIENTS] = {};
+
+public:
+    void MarkTeeDead(int ClientId);
+
+    float GetPlayerTargetAngle(
+        const CNetObj_Character *pPrevChar,
+        const CNetObj_Character *pPlayerChar,
+        int ClientId,
+        float Intra = 0.0f);
+
+    int Sizeof() const override
+    {
+        return sizeof(*this);
+    }
+
+    void OnInit() override;
+    void OnRender() override;
+
+    const std::shared_ptr<CManagedTeeRenderInfo> &NinjaTeeRenderInfo() const
+    {
+        return m_pNinjaTeeRenderInfo;
+    }
+
+    const std::shared_ptr<CManagedTeeRenderInfo> &SpectatorTeeRenderInfo() const
+    {
+        return m_pSpectatorTeeRenderInfo;
+    }
+};
+
+#endif
